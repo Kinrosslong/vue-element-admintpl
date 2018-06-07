6fa1e757cb78a54693977e8f6f6d6deddf672c09
Kinrosslong <longkinross@gmail.com>
2018-06-07 17:39:30 +0800

    vue-element-admin 后台模板

---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,149 @@
+<template>
+    <div class="sidebar">
+        <el-menu class="sidebar-el-menu" :default-active="onRoutes" :collapse="collapse" background-color="#2A3F54"
+            text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened router>
+            <template v-for="item in items">
+                <template v-if="item.subs">
+                    <el-submenu :index="item.index" :key="item.index">
+                        <template slot="title">
+                            <i :class="item.icon"></i><span slot="title">{{ item.title }}</span>
+                        </template>
+                        <el-menu-item v-for="(subItem,i) in item.subs" :key="i" :index="subItem.index">
+                           <i :class="subItem.icon"></i> {{ subItem.title }}
+                        </el-menu-item>
+                    </el-submenu>
+                </template>
+                <template v-else>
+                    <el-menu-item :index="item.index" :key="item.index">
+                        <i :class="item.icon"></i><span slot="title">{{ item.title }}</span>
+                    </el-menu-item>
+                </template>
+            </template>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+    import bus from '../common/bus';
+    export default {
+        data() {
+            return {
+                collapse: false,
+                items: [
+                    {
+                        icon: 'el-icon-setting',
+                        index: 'dashboard', //这个index 是router下的路由名称
+                        title: '系统首页'
+                    },
+                    {
+                        icon: 'el-icon-tickets',
+                        index: '2',
+                        title: '基础表格',
+                        subs: [
+                            {                 
+                                icon: 'ali-el-icon-text',
+                                index: 'table',
+                                title: '表格'
+                            },
+                            {
+                                icon: 'ali-el-icon-text',
+                                index: 'table1',
+                                title: '表格1'
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'el-icon-message',
+                        index: '3',
+                        title: 'tab选项卡',
+                        subs: [
+                            {                 
+                                icon: 'ali-el-icon-viewgallery',
+                                index: 'tabs',
+                                title: 'tab选项卡'
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'el-icon-date',
+                        index: '4',
+                        title: '表单相关',
+                        subs: [
+                            {   
+                                icon: 'ali-el-icon-text',
+                                index: 'form',
+                                title: '基本表单'
+                            },
+                            {
+                                icon: 'ali-el-icon-navlist',
+                                index: 'editor',
+                                title: '富文本编辑器'
+                            },
+                            {
+                                icon: 'ali-el-icon-viewlist',
+                                index: 'markdown',
+                                title: 'markdown编辑器'
+                            },
+                            {
+                                icon: 'ali-el-icon-shangchuan',
+                                index: 'upload',
+                                title: '文件上传'
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'el-icon-star-on',
+                        index: 'charts',
+                        title: 'schart图表'
+                    },
+                    {
+                        icon: 'el-icon-rank',
+                        index: 'drag',
+                        title: '拖拽列表'
+                    },
+                    {
+                        icon: 'el-icon-warning',
+                        index: 'permission',
+                        title: '权限测试'
+                    },
+                    {
+                        icon: 'el-icon-error',
+                        index: '404',
+                        title: '404页面'
+                    }
+                ]
+            }
+        },
+        computed:{
+            onRoutes(){
+                return this.$route.path.replace('/','');
+            }
+        },
+        created(){
+            // 通过 Event Bus 进行组件间通信，来折叠侧边栏
+            bus.$on('collapse', msg => {
+                this.collapse = msg;
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    .sidebar{
+        display: block;
+        position: absolute;
+        left: 0;
+        top: 70px;
+        bottom:0;
+        overflow-y: scroll;
+    }
+    .sidebar::-webkit-scrollbar{
+        width: 0;
+    }
+    .sidebar-el-menu:not(.el-menu--collapse){
+        width: 250px;
+    }
+    .sidebar > ul {
+        height:100%;
+    }
+</style>
